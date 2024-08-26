16da79e12778490122daf9d12d33720b5c04f7e5
Fernando Aparicio <faparicior@gmail.com>
2024-08-26 19:48:01 +0200

    limit with stream

--- polars_demo.py
+++ polars_demo.py
@@ -3,12 +3,16 @@ import polars as pl
 
 start_time1 = time.time()
 
-# data = pl.scan_csv('/tmp/data/discogs.csv')
-data = pl.scan_csv('./data/discogs.csv')
-result = data.group_by('artist_name').len()
+data = pl.scan_csv('/tmp/data/discogs.csv', low_memory=True, rechunk=False)
+# data = pl.scan_csv('./data/discogs.csv')
+result = data.group_by('artist_name').len().select('artist_name')
 
-# print(result.collect(streaming=True))
-print(result.collect())
+print(result.collect(streaming=True))
+#
+# for batch in result.collect(streaming=True):
+#     print(batch)
+
+# print(result.collect())
 
 end_time1 = time.time()
 print(f"Time taken: {end_time1 - start_time1:.2f} seconds")
